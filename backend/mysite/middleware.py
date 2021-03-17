f493c59e4fcd5e9e7c5f0c2f5d867919c5ade8e7
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2021-03-16 17:01:02 -0700

    Switch allowed request origins based on environment

--- backend/mysite/middleware.py
+++ backend/mysite/middleware.py
@@ -1,3 +1,6 @@
+import os
+
+
 class CorsMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
@@ -5,18 +8,14 @@ class CorsMiddleware(object):
     def __call__(self, request):
         response = self.get_response(request)
         
-        # TODO: Allow this for develop mode, and switch on a setting
-        
-        # Needs to be commented out for production build
-        # response["Access-Control-Allow-Origin"] = "http://localhost:8080"
-        # response["Access-Control-Allow-Origin"] = "http://jesse-memorial:8080"
-
-        # Needs to be commented out for local development 
-        response["Access-Control-Allow-Origin"] = "http://celebrating-jesse-frontend.s3-website-us-west-2.amazonaws.com"
-        
-        # response["Access-Control-Allow-Origin"] = "*"
+        if os.getenv('GAE_APPLICATION', None):
+            # Production allowed request origins
+            response["Access-Control-Allow-Origin"] = "http://celebrating-jesse-frontend.s3-website-us-west-2.amazonaws.com"
+        else:
+            # Local development allowed request origins
+            # response["Access-Control-Allow-Origin"] = "http://localhost:8080"
+            response["Access-Control-Allow-Origin"] = "http://jesse-memorial:8080"
         
         response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-        # response["Access-Control-Allow-Headers"] = "Accept, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Access-Control-Request-Method, Authorization, Connection, Content-Type, Host, Origin, Referer, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent"
         response["Access-Control-Allow-Methods"] = "GET, POST, PATCH, PUT, DELETE, OPTIONS"
         return response
